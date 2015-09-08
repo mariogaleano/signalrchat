df20872d5107868f982b2a707bbbffeb7de7da10
mariogaleano <mariogaleano@outlook.com>
2015-09-08 11:35:47 -0500

    nlog

--- SignalRchat/ChatHub.cs
+++ SignalRchat/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.SignalR;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,11 @@ namespace SignalRchat
 {
     public class ChatHub : Hub
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public void Send(string name, string message)
         {
+            Logger.Info(string.Format("{0} : {1}",name,message));
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
         }
